eb7259edeff7b962dba8f000c9e3f75d5bf12433
Rose Khaldeeva <khalrose@mail.ru>
2023-02-13 22:08:17 +0600

    Initial Commit

---
+++ Quizzler1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler1.0
+//
+//  Created by Rose on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
